bda1d146abd2fe69996c1c25d1f14a63371b545f
Bogdan <skobogdan@gmail.com>
2014-06-14 22:49:04 +0300

    address commit

--- src/Address.java
+++ src/Address.java
@@ -1,2 +1,10 @@
 public class Address {
+
+    // 1 some new line for test
+    // 2 first do local commit
+    // 3 then push local changes to remote repository
+    // if file is shown as red it means that git doesnt see for file
+    // Blue - file was changed but not commited
+
+
 }
